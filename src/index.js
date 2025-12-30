ab8e9125cb9ce5e55ebe2c88ac4ad50b9585d7d0
Sarthak Singh Chaudhary <Sarthaksinghchaudhary0@gmail.com>
2025-12-30 22:09:16 +0530

    added user and video model

--- src/index.js
+++ src/index.js
@@ -27,4 +27,14 @@ dotenv.config({
     path: "./.env"
 });
     
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch(
+    (error) => {
+        console.error("MOngo DB connection failed: ", error);
+    }
+);
